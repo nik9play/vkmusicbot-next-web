268fb2d7a6f317fccf49e8eb73b47df3c37a10cb
nik9 <n-bogun@mail.ru>
2021-06-12 21:27:15 +0300

    remove old plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/vue-tilt.js',
-    { src: '~/plugins/vue-apex.js', ssr: false },
     '~/plugins/vue-clipboard.js'
   ],
 
